1af6c3f4e54a988744ce1452fe8d4fb5b37be1da
Lucian <lsavca377@gmail.com>
2024-11-21 22:43:01 +0200

    deploy react to github

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/pulsefit-deploy-github/",
   plugins: [react()],
-  base: '/pulsefit-deploy/'
 })
